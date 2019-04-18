029831d1ea8853e3fde1c30586f0164e528137cf
core.editor <andreeavillatte@gmail.com>
2019-04-18 11:58:42 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //si je clique sur le bouton erase
+    $('#erase').click(function () {
+        //j'efface le contenu du 2eme paragraphe
+        $('#secondText').empty();
+    });
+});
